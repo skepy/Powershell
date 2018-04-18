ffc61d3831411e88062128895a5265ce3db74831
skepy <emkxy@protonmail.com>
2018-04-18 21:53:33 +0100

    Commit
    Scan a directory for content;
    Name
    Creation
    Directory
    Shared Permissions
    Inherited Permissions
    Extension

---
+++ FolderContents.ps1
@@ -0,0 +1,17 @@
+﻿Get-ChildItem -Path dir -Recurse |`
+foreach{
+$Item = $_
+$Age = $_.CreationTime
+$Path = $_.FullName
+$Type = $_.Extension
+$Folder = $_.PSIsContainer
+
+
+$Path | Select-Object `
+    @{n="Name";e={$Item}},`
+    @{n="Created";e={$Age}},`
+    @{n="filePath";e={$Path}},`
+    @{n='Permissions';e={$Access.FileSystemRights}},`
+    @{n='Inherited';e={$Access.IsInherited}},`
+    @{n="Extension";e={if($Folder){"Folder"}else{$Type}}}`
+}| Export-Csv dir -NoTypeInformation 
